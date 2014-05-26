6696ab71eb4f3ce66b2cb16f39107f852ec23ffa
Michael Gruener <michael.gruener@to.com>
2014-05-26 19:15:45 +0200

    Use the new function sequence_string to manage a directory structure for the hello world textfile

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,11 @@
 class testguide ( $world ) {
-  file { '/tmp/hello.txt':
+  $path = 'some/useless/directory'
+  $directories = sequence_string($path,'/','/tmp/')
+  file { $directories:
+    ensure => directory,
+  }
+
+  file { "/tmp/${path}/hello.txt":
     ensure  => file,
     content => template('testguide/hello.erb')
   }
