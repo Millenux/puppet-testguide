3c775e9e233adb0f824ee97f70ba08910b45ffc1
Michael Gruener <michael.gruener@to.com>
2014-05-26 19:15:06 +0200

    Add a custom function plugin

---
+++ lib/puppet/parser/functions/sequence_string.rb
@@ -0,0 +1,25 @@
+module Puppet::Parser::Functions
+  newfunction(:sequence_string, :type => :rvalue, :doc => <<-EOS
+Takes a string and returns the sequence substr1,substr1substr2,substr1substr2substr3,...
+
+*Example*
+
+    sequence_string("/this/is/a/test","/")
+
+Would result in:
+
+    ["/","/this/","/this/is/","/this/is/a/","/this/is/a/test"]
+    EOS
+  ) do |arguments|
+    str = arguments[0]
+    separator = arguments[1]
+    sequence = [arguments[2]]
+    str.each_line(separator) do |part|
+      sequence << "#{sequence[-1]}#{part}"
+    end
+    if arguments[2]
+      sequence.shift
+    end
+    return sequence.compact
+  end
+end
